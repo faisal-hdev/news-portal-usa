6addb3753508a05a08b1ea2c3aade62d0a6c1be0
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-03-02 03:30:57 +0600

    Click event to dynamic data show to the card emplement are added

--- js/app.js
+++ js/app.js
@@ -8,38 +8,46 @@ const loadCategories = async () => {
   const data = await response.json();
   const accessApiData = data.data.news_category;
   accessApiData.forEach((singleCategory) => {
-    const newBtn = document.createElement("button");
-    newBtn.className =
-      "bg-indigo-200 hover:text-white hover:bg-indigo-400 duration-300 text-black max-sm:px-2 px-5 py-2 cursor-pointer rounded-lg";
-    newBtn.innerText = singleCategory?.category_name;
-    categoryContainerEl.appendChild(newBtn);
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+      <button onclick="loadNews('${singleCategory.category_id}')"
+            class="bg-indigo-200 hover:text-white hover:bg-indigo-400 duration-300 text-black max-sm:px-2 px-5 py-2 cursor-pointer rounded-lg"
+          >
+        ${singleCategory.category_name}
+      </button>
+    `;
+    categoryContainerEl.appendChild(newDiv);
   });
 };
 
-const loadNews = async () => {
-  const url = "https://openapi.programming-hero.com/api/news/category/01";
+const loadNews = async (catId) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${catId}`;
   const response = await fetch(url);
   const data = await response.json();
   const newsData = data.data;
+  CardContainerEl.innerHTML = "";
+  console.log(newsData);
   newsData.forEach((singleNews) => {
-    console.log(singleNews);
-
-    //   card card-side
+    if (newsData.length === 0) {
+      alert("hello");
+    }
     const newDiv = document.createElement("div");
     newDiv.innerHTML = `
-        <div class=" max-sm:w-auto max-w-[800px] flex max-sm:flex-col bg-base-100 shadow-md border">
+        <div class="max-sm:w-auto md:w-[900px] flex max-sm:flex-col bg-base-100 shadow-md border">
             <div class='flex items-center justify-center'>
               <img class='p-4' src="${singleNews?.thumbnail_url}" alt="Movie"/>
             </div>
-            <div class="card-body w-[550px]">
+            <div class="card-body max-sm:w-auto md:w-[550px]">
                 <div class='flex max-sm:flex-col'>
-                    <h2 class="card-title">${singleNews?.title}</h2>
+                    <h2 class="card-title max-sm:text-center">${
+                      singleNews?.title
+                    }</h2>
                     <div class='text-center'>
                         <p>${singleNews?.rating?.badge}</p>
                         <p>${singleNews?.rating?.number}</p>
                     </div>
                 </div>
-                <p>${singleNews?.details.slice(
+                <p class='max-sm:text-center'>${singleNews?.details.slice(
                   0,
                   180
                 )}.....<span class='text-[14px]'>see more</span></p>
@@ -72,5 +80,5 @@ const loadNews = async () => {
   });
 };
 
-loadNews();
+loadNews("01");
 loadCategories();
